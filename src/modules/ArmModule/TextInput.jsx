ef5ae46fccd932dc2e9d1074b6b4d7dd7d4f5f5a
Evan Ugarte <evanuxd@gmail.com>
2019-02-22 19:15:49 -0800

    Changed Arm Module to Fit Firmware
    Now the parameters match, we only need to add the delta param. Tested with control systems and commands are received.

--- src/modules/ArmModule/TextInput.jsx
+++ src/modules/ArmModule/TextInput.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
-import { InputGroup, Input, InputGroupAddon, Col, Row, Button, ButtonGroup } from "reactstrap";
-import Presets from "./Presets";
+import { InputGroup, Input, InputGroupAddon, Col } from "reactstrap";
 import "./ArmStyle.css";
 
 class SliderView extends Component {
   state = {
-    armInputs: [
-      { name: "Rotunda", id: "target", value: null },
-      { name: "Elbow", id: "target", value: null },
-      { name: "Shoulder", id: "target", value: null },
-    ],
+    armInputs: ["Rotunda", "Elbow", "Shoulder"],
     wristInputs: [
       { name: "Dimension", id: "wrist", type: "" },
       { name: "Delta", id: "wrist", type: "" },
@@ -17,57 +12,36 @@ class SliderView extends Component {
   };
 
   handleKeyPress = (e) => {
-    if (e.target.id === "target") {
-      let jeff = `${e.target.name}Target`;
-      if (e.key === "Enter") {
-        this.props.handleXHR({ [jeff]: e.target.value });
-      }
+    let jeff = `${e.target.name}Target`;
+    if (e.key === "Enter") {
+      this.props.handleXHR({ [jeff]: e.target.value });
     }
   }
 
   render() {
     return (
       <div id="controls">
-        <Row>
           <Col>
             {this.state.armInputs.map((input) => {
               return (
-                <React.Fragment key={input.id}>
-                  <InputGroup key={input.id}>
-                    <InputGroupAddon key={input.id} addonType="append">
-                      {input.name}
+                <React.Fragment key={input}>
+                  <InputGroup key={input}>
+                    <InputGroupAddon key={input} addonType="append">
+                      {input}
                     </InputGroupAddon>
                     <Input
                       onKeyPress={this.handleKeyPress}
-                      id={input.id}
-                      name={input.name}
+                      id={input}
+                      name={input}
                       type="number"
                       step="0.01"
-                      placeholder="Input Servo Here..."
+                      placeholder="Input Value Here..."
                     />
                   </InputGroup>
                 </React.Fragment>
               );
             })}
           </Col>
-          <Col>
-            <h3>Wrist</h3>
-            {this.state.wristInputs.map((element) => {
-              return (
-                <React.Fragment key={element.id}>
-                  <p>{element.name}</p>
-                  <Row>
-                    <ButtonGroup>
-                      <Button color="primary" id={element.id} value={0} /*onClick={} active={}*/>0</Button>
-                      <Button color="primary" value={1}/*onClick={} active={}*/>1</Button>
-                    </ButtonGroup>
-                  </Row>
-                </React.Fragment>
-              );
-            })}
-          </Col>
-        </Row>
-        <Presets handleXHR={this.props.handleXHR} />
       </div>
     );
   }
