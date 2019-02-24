70a063ad02156e12047e2f20264bfb0227799c0e
Aris Koumis <ariskoumis@gmail.com>
2019-02-24 02:15:00 -0800

    SAR-Ready Video Stream Module

--- src/modules/VideoStream/VideoStream.jsx
+++ src/modules/VideoStream/VideoStream.jsx
@@ -13,9 +13,10 @@ import {
   FaCheck,
   FaExclamationTriangle
 } from "react-icons/fa"
+
 import "./VideoStreamStyle.css";
 
-const DEFAULT_IMG_PATH = "https://ahseeit.com//king-include/uploads/2019/02/51508006_1207326942754330_6970094343420161442_n-3244417952.jpg";
+import default_stream_src from './assets/images/default_stream_src.jpg';
 
 class VideoStreamModule extends Component {
   constructor(props) {
@@ -27,7 +28,7 @@ class VideoStreamModule extends Component {
 
   componentDidMount = () => {
     let video_component = document.getElementById("video");
-    video_component.src = DEFAULT_IMG_PATH;
+    video_component.src = default_stream_src;
   }
 
   updateSource = () => {
@@ -51,7 +52,7 @@ class VideoStreamModule extends Component {
     alert("ERROR: Connection IP address was unsuccessful. Defaulting source.");
 
     let video_component = document.getElementById("video");
-    video_component.src = DEFAULT_IMG_PATH;
+    video_component.src = default_stream_src;
 
     this.setState({
       connected: false
