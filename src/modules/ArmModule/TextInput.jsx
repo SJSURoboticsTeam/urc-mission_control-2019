36c1ca64d7cffc7e1c8c6f69f9aac1e42d5c7dbd
Evan Ugarte <evanuxd@gmail.com>
2019-04-24 08:36:56 -0700

    Restructured Arm Module for Wrist
    From Zach's reccomendations, I changed the arm module to have wrist
    pitch and wrist roll inputs. We also moved some components around so the
    limited space could be better used.

--- src/modules/ArmModule/TextInput.jsx
+++ src/modules/ArmModule/TextInput.jsx
@@ -1,38 +1,69 @@
 import React, { Component } from "react";
-import { InputGroup, Input, InputGroupAddon, Col } from "reactstrap";
+import { InputGroup, Input, InputGroupAddon, Col, Row } from "reactstrap";
 import "./ArmStyle.css";
 
 class SliderView extends Component {
   state = {
     armInputs: ["Rotunda", "Elbow", "Shoulder"],
     wristInputs: [
-      { name: "Dimension", id: "wrist", type: "" },
-      { name: "Delta", id: "wrist", type: "" },
+      { name: "Pitch", id: "wrist" },
+      { name: "Roll", id: "wrist" }
     ]
   };
 
-  handleKeyPress = (e) => {
-    let jeff = `${e.target.name}Target`;
-    if (e.key === "Enter") {
-      this.props.handleXHR({ [jeff]: e.target.value });
+  handleArmKeyPress = (e) => {
+    if(e.key === "Enter") {
+      if(e.target.id !== "wrist") {
+        let jeff = `${e.target.name}Target`;
+        this.props.handleXHR({ [jeff]: e.target.value });
+        console.log(`${jeff} = ${e.target.value}`)
+      } else {
+        let jeff = `Wrist${e.target.name}`;
+        this.props.handleXHR({ [jeff]: e.target.value });
+        console.log(`${jeff} = ${e.target.value}`)
+      }
     }
   }
 
   render() {
     return (
       <div id="controls">
+        <Row>
           <Col>
-            {this.state.armInputs.map((input) => {
+            <p>Arm Inputs</p>
+              {this.state.armInputs.map((input) => {
+                return (
+                  <React.Fragment key={input}>
+                    <InputGroup key={input}>
+                      <InputGroupAddon key={input} addonType="append">
+                        {input}
+                      </InputGroupAddon>
+                      <Input
+                        onKeyPress={this.handleArmKeyPress}
+                        id={input}
+                        name={input}
+                        type="number"
+                        step="0.01"
+                        placeholder="Input Value Here..."
+                      />
+                    </InputGroup>
+                  </React.Fragment>
+                );
+              })}
+          </Col>
+          <Col>
+          <p>Wrist Inputs</p>
+            {this.state.wristInputs.map((input) => {
               return (
-                <React.Fragment key={input}>
-                  <InputGroup key={input}>
+                <React.Fragment key={input.name}>
+                  <InputGroup key={input.name}>
                     <InputGroupAddon key={input} addonType="append">
-                      {input}
+                      {input.name}
                     </InputGroupAddon>
                     <Input
-                      onKeyPress={this.handleKeyPress}
-                      id={input}
-                      name={input}
+                      onKeyPress={this.handleArmKeyPress}
+                      id="wrist"
+                      name={input.name}
                       type="number"
                       step="0.01"
                       placeholder="Input Value Here..."
@@ -42,6 +73,7 @@ class SliderView extends Component {
               );
             })}
           </Col>
+        </Row>
       </div>
     );
   }
