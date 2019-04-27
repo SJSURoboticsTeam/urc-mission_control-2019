c77de73aeb46626bf754b3b1bea8d4ccc8feae30
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2019-04-27 14:05:54 -0700

    Complete all functionality for Mast Camera. (#52)

--- src/modules/Common/ModuleContainer.jsx
+++ src/modules/Common/ModuleContainer.jsx
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie";
 import ModuleSelect from "./ModuleSelect.jsx";
 import "../../lib/css/ModuleContainer.css";
 import ProtoModule from "../ProtoModule/ProtoModule.jsx";
+import MastModule from "../MastModule/MastModule.jsx";
 import ArmModule from "../ArmModule/ArmModule.jsx";
 import ScienceModule from "../Science/ScienceModule.jsx";
 import PowerModule from "../Power/PowerModule.jsx";
@@ -47,6 +48,8 @@ class ModuleContainer extends Component {
     switch (moduleName) {
       case "proto-module":
         return <ProtoModule />;
+      case "mast-module":
+        return <MastModule />;
       case "arm-module":
         return <ArmModule />;
       case "science-module":
